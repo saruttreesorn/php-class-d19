44fdbaed9e35da0ece76972e2e0ada82196741c7
johannes <johannes.muljana@ait.nsw.edu.au>
2019-10-02 20:01:36 +1000

    updating shopping cart code

--- classes/ShoppingCart.php
+++ classes/ShoppingCart.php
@@ -4,23 +4,24 @@ namespace aitsydney;
 use aitsydney\Database;
 
 class ShoppingCart extends Database{
-  private $auth_status;
+  private $response = array();
+  private $errors = array();
 
   public function __construct(){
     parent::__construct();
-    $this -> initSession();
   }
-
-  private function initSession(){
+  private function getUserAuthStatus(){
     if( session_status() == PHP_SESSION_NONE ){
       session_start();
-      $this -> auth_status = ( isset($_SESSION['auth']) ) ? true : false;
     }
+    return ( isset($_SESSION['auth'] ) ) ? $_SESSION['auth'] : false ;
   }
-  private function setCartId(){
-    
+
+  private function getUserCartId( $account_id, $createnew = false ){
+    //get the user's cart id or return a new one
   }
-  
+
+  // add an item to the cart
   public function setItem( $product_id, $quantity ){
 
   }
